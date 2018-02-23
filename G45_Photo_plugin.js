9bdd5d38af150f811ec37cc1ad23beb63f585538
greg <gregcj@hotmail.co.uk>
2018-02-23 13:41:25 +0000

    Added Check box input showing.
    Still need to add the arguments.

--- G45_Photo_plugin.js
+++ G45_Photo_plugin.js
@@ -9,6 +9,71 @@ var i=0;
 //Format json.
 var config;
 
+//Make search argument fields based on checkboxes ticked.
+$(document).ready(function()
+{
+  //State variables used to prevent duplication of fields.
+  var s1,s2,s3,s4 = false;
+
+  //Add both min upload date and min taken date fields.
+  $("#Dates").click(function()
+  {
+    if(s1 == true)
+    {
+      $("#DatesDiv").remove();
+      s1 = false;
+    }
+    else
+    {
+      $("#Search").append("<div id='DatesDiv'><label for='minUpLDate'>Minimum Upload Date:</label><input id='minUpLDate' type='date'><br><label for='minTaDate'>Minimum Taken Date:</label><input id='minTaDate' type='date'><br></div>")
+      s1 = true;
+    }
+  })
+
+  //Add both lat + long fields.
+  $("#LongLat").click(function()
+  {
+    if(s2 == true)
+    {
+      $("#LongLatDiv").remove();
+      s2 = false;
+    }
+    else
+    {
+      $("#Search").append("<div id='LongLatDiv'><label for='Lat'> Latitude </label><input id='Lat' type='text'><br><label for='Long'> Longitude </label><input id='Long' type='text'><br></div>")
+      s2 = true;
+    }
+  })
+
+  $("#Poi").click(function()
+  {
+    if(s3 == true)
+    {
+      $("#PoiDiv").remove();
+      s3 = false;
+    }
+    else
+    {
+      $("#Search").append("<div id='PoiDiv'><label for='poi'> Place of Interest </label><input id='poi' type='text'><br></div>")
+      s3 = true;
+    }
+
+  })
+
+  $("#Tags").click(function()
+  {
+    if(s4 == true)
+    {
+      $("#TagDiv").remove();
+      s4 = false;
+    }
+    else
+    {
+      $("#Search").append("<div id='TagDiv'><label for='tags'> Comma separated tag </label><input id='tags' type='text'><br></div>")
+      s4 = true;
+    }
+  })
+});
 
 //DEBUG birmingham weoid: 12723
 //Base URL:https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=7e60cf64ebe42702907fa713ab085a05
@@ -17,17 +82,10 @@ function getPhotos()
   var args;
 
   //Add arguments
-  if(document.getElementById("dates").checked = true)
-  {
-    if(document.getElementById("minUpLDate").value !== null)
-    {
-      args = args + "&min_upload_date=" + document.getElementById("minUpLDate").value;
-    }
-    if(document.getElementById("minTaDate").value !== null)
-    {
-      args = args + "&min_taken_date=" + document.getElementById("minTaDate").value;
-    }
-  }
+
+  args = args + "&min_upload_date=" + $("#minUpLDate").val();
+
+  console.log(args);
   $.ajax({
     //Base Search method with key
     url: "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=e9d832b772414144a351d458238065d4"
